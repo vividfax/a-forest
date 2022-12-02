ed7688261d639bbd5af12f93c6a2960a9a201750
vividfax <vividfax@gmail.com>
2022-12-02 00:25:35 +0000

    Minor aesthetic fix

--- js/Mailbox.js
+++ js/Mailbox.js
@@ -113,7 +113,7 @@ class Mailbox {
         } else if (this.side == -1) {
 
             rectMode(CENTER);
-            fill("#F2F2F2");
+            fill("#eee");
             rect(0, 0, this.w+20, this.h+20);
             image(this.postcardPhotoImage, 0, 0);
         }
